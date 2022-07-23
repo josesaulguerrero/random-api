0636934b852bf65313daaa4960495d78aa6c83d3
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-22 19:10:20 -0500

    Added the Person abstract class

--- src/main/java/co/com/randomapi/persistence/entity/Person.java
+++ src/main/java/co/com/randomapi/persistence/entity/Person.java
@@ -1,4 +1,21 @@
 package co.com.randomapi.persistence.entity;
 
-public abstract class Person {
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public abstract class Person<T> {
+    protected T id;
+    protected String DNI;
+    protected String name;
+    protected Integer age;
+
+    protected Person(String DNI, String name, Integer age) {
+        this.DNI = DNI;
+        this.name = name;
+        this.age = age;
+    }
 }
