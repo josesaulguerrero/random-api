a7cb9a90136924d9f72849c61ffa633e192a6021
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-23 13:55:52 -0500

    Oops, forgot to add lombok annotations

--- src/main/java/co/com/randomapi/domain/dto/StudentDTO.java
+++ src/main/java/co/com/randomapi/domain/dto/StudentDTO.java
@@ -1,13 +1,26 @@
 package co.com.randomapi.domain.dto;
 
 import co.com.randomapi.persistence.entity.Subject;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentDTO {
     private long id;
     private String name;
     private int age;
     private String dni;
     private Set<Subject> subscribedTo;
+
+    public StudentDTO(String name, int age, String dni, Set<Subject> subscribedTo) {
+        this.name = name;
+        this.age = age;
+        this.dni = dni;
+        this.subscribedTo = subscribedTo;
+    }
 }
