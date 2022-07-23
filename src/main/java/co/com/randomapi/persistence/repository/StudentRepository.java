ddb227d73707eb0c4c24062513a107911c05ad77
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-23 11:19:24 -0500

    Created the StudentRepository

--- src/main/java/co/com/randomapi/persistence/repository/StudentRepository.java
+++ src/main/java/co/com/randomapi/persistence/repository/StudentRepository.java
@@ -1,4 +1,7 @@
 package co.com.randomapi.persistence.repository;
 
-public interface StudentRepository {
+import co.com.randomapi.persistence.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
 }
