5275126bcd6e0fbe2f974215ea44b5f1c6596f00
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-23 09:22:38 -0500

    Updated Subject and created the SubjectDifficulty Enum

--- src/main/java/co/com/randomapi/persistence/entity/Subject.java
+++ src/main/java/co/com/randomapi/persistence/entity/Subject.java
@@ -13,8 +13,15 @@ import javax.persistence.*;
 @Table(name = "subjects")
 public class Subject {
     @Id
+    @Column(name = "id", unique = true)
     private Long Id;
 
+    private String name;
+
+    @Enumerated(value = EnumType.STRING)
+    private SubjectDifficulty difficulty;
+
     @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "FK_professor")
     private Professor professorInCharge;
 }
---
+++ src/main/java/co/com/randomapi/persistence/entity/SubjectDifficulty.java
@@ -0,0 +1,2 @@
+package co.com.randomapi.persistence.entity;public enum SubjectDifficulty {
+}
