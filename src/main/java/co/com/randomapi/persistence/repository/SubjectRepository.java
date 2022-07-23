2bd1b6f26948d9ec423c287a9bad370ce35bd9c4
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-23 11:20:07 -0500

    Created the SubjectRepository

--- src/main/java/co/com/randomapi/persistence/repository/SubjectRepository.java
+++ src/main/java/co/com/randomapi/persistence/repository/SubjectRepository.java
@@ -1,4 +1,7 @@
 package co.com.randomapi.persistence.repository;
 
-public interface SubjectRepository {
+import co.com.randomapi.persistence.entity.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
 }
